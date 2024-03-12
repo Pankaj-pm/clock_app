b21b499f5b44456c8e2e81a22c093c412e318a7a
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-03-12 08:57:11 +0530

    Future.delayed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,67 +9,125 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int maxLine = 2;
+  bool isLoad = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: SafeArea(
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-
-                },
-                overlayColor: MaterialStatePropertyAll(Colors.blue),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.file_copy_sharp),
-                    Text("File",style: TextStyle(fontSize: 18),),
-                  ],
-                ),
+      // drawer: Drawer(
+      //   child: SafeArea(
+      //     child: Column(
+      //       children: [
+      //         InkWell(
+      //           onTap: () {},
+      //           overlayColor: MaterialStatePropertyAll(Colors.blue),
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //             children: [
+      //               Icon(Icons.file_copy_sharp),
+      //               Text(
+      //                 "File",
+      //                 style: TextStyle(fontSize: 18),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Icon(Icons.file_copy_sharp),
+      //             Text(
+      //               "File",
+      //               style: TextStyle(fontSize: 18),
+      //             ),
+      //           ],
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Icon(Icons.file_copy_sharp),
+      //             Text(
+      //               "File",
+      //               style: TextStyle(fontSize: 18),
+      //             ),
+      //           ],
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Icon(Icons.file_copy_sharp),
+      //             Text(
+      //               "File",
+      //               style: TextStyle(fontSize: 18),
+      //             ),
+      //           ],
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Icon(Icons.file_copy_sharp),
+      //             Text(
+      //               "File",
+      //               style: TextStyle(fontSize: 18),
+      //             ),
+      //           ],
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Icon(Icons.file_copy_sharp),
+      //             Text(
+      //               "File",
+      //               style: TextStyle(fontSize: 18),
+      //             ),
+      //           ],
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      drawer: NavigationDrawer(
+        children: [
+          // DrawerHeader(
+          //   child: Text("Hello"),
+          // ),
+          UserAccountsDrawerHeader(
+            accountName: Text("Flutter"),
+            accountEmail: Text("Flutter@gmail.com"),
+            otherAccountsPictures: [
+              CircleAvatar(
+                child: Text("A"),
+                backgroundImage: NetworkImage("https://androidknowledge.com/wp-content/uploads/2022/10/navthumb.png"),
               ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.file_copy_sharp),
-                  Text("File",style: TextStyle(fontSize: 18),),
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.file_copy_sharp),
-                  Text("File",style: TextStyle(fontSize: 18),),
-                ],
-              ),SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.file_copy_sharp),
-                  Text("File",style: TextStyle(fontSize: 18),),
-                ],
-              ),SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.file_copy_sharp),
-                  Text("File",style: TextStyle(fontSize: 18),),
-                ],
-              ),SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.file_copy_sharp),
-                  Text("File",style: TextStyle(fontSize: 18),),
-                ],
-              ),
+              CircleAvatar(
+                child: Text("A"),
+                backgroundImage: NetworkImage("https://androidknowledge.com/wp-content/uploads/2022/10/navthumb.png"),
+              )
             ],
+            currentAccountPicture: CircleAvatar(
+              child: Text("A"),
+              backgroundImage: NetworkImage("https://androidknowledge.com/wp-content/uploads/2022/10/navthumb.png"),
+            ),
           ),
-        ),
+          Text("Hello"),
+          Divider(),
+          Text("Hello"),
+        ],
       ),
       appBar: AppBar(),
       body: Column(
@@ -94,7 +152,15 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           TextButton(
-            onPressed: () {},
+            onPressed: () async{
+              isLoad = true;
+              setState(() {});
+              await Future.delayed(Duration(seconds: 3),() {
+
+              },);
+              isLoad = false;
+              setState(() {});
+            },
             style: ButtonStyle(
                 // overlayColor: MaterialStatePropertyAll(Colors.red),
                 textStyle: MaterialStatePropertyAll(TextStyle(fontSize: 12))),
@@ -102,15 +168,58 @@ class _HomePageState extends State<HomePage> {
           ),
           ElevatedButton(
             onPressed: () {},
-
-            child: Text("Click"),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.add),
+                Text("Click"),
+              ],
+            ),
           ),
           IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit)),
           OutlinedButton(onPressed: () {}, child: Text("Click")),
+          TextButton.icon(onPressed: () {}, icon: Icon(Icons.ac_unit), label: Text("Add")),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: SizedBox(
+                height: 10,
+                width: 10,
+                child: CircularProgressIndicator(),
+              ),
+              label: Text("Add")),
+          Container(
+            height: 150,
+            width: 150,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+            clipBehavior: Clip.antiAlias,
+            child: isLoad
+                ? Center(
+                    child: CircularProgressIndicator(),
+
+                  )
+                : Image.network(
+                    "https://androidknowledge.com/wp-content/uploads/2022/10/navthumb.png",
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          Duration d = Duration(seconds: 2);
+          print(d.inHours);
+          print("Order..");
+          await Future.delayed(
+            Duration(seconds: 3),
+            () {
+              print("Call after 3 sec");
+            },
+          );
+
+          print("Order Completed");
+        },
       ),
     );
   }
